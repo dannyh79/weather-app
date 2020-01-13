6975a6bdd086bf29dff4f92515810b447ad7e8d2
danny-ch han <chenghsuan@5xruby.tw>
2020-01-13 23:55:43 +0800

    Add weather search feature for App

--- src/App.js
+++ src/App.js
@@ -1,64 +1,95 @@
-import React from 'react';
+// Credit: https://www.youtube.com/watch?v=GuA0_Z1llYU
+
+import React, { useState, useEffect } from 'react';
 
 const api = {
   key: '664eac603809f1426027912a55140749',
-  url: 'https://api.openweathermap.org/data/2.5/'
+  urlBase: 'https://api.openweathermap.org/data/2.5/weather'
 }
 
 function App() {
+  const defaultCity = 'Taipei'
+  const [city, setCity] = useState(defaultCity)
+  const [cityInfo, setCityInfo] = useState({})
+  useEffect(() => {
+    if (city !== '') {
+      fetch(`${api.urlBase}?q=${city}&units=metric&APPID=${api.key}`)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json()
+          } else {
+            throw new Error(`${response.statusText}: ${city}`);
+          }
+        })
+        .then((result) => {
+          setCityInfo(result)
+          setCity('')
+        })
+        .catch((error) => alert(error))
+    }
+  }, [city])
+
   function dateBuilder(d) {
     const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
+      'January', 'February', 'March',
+      'April', 'May', 'June', 'July',
+      'August', 'September', 'October',
+      'November', 'December'
     ]
     const weekdays = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
+      'Sunday', 'Monday', 'Tuesday',
+      'Wednesday', 'Thursday', 'Friday',
       'Saturday'
     ]
 
-    const day = weekdays[d.getDay()]
+    const weekday = weekdays[d.getDay()]
     const date = d.getDate()
     const month = months[d.getMonth()]
     const year = d.getFullYear()
 
-    return `${day} ${date} ${month} ${year}`
+    return `${weekday} ${date} ${month} ${year}`
+  }
+
+  function shouldNotBeEmpty(object) {
+    return Object.keys(object).length !== 0
+  }
+
+  function shouldWeatherBeWarm(temp) {
+    const coldTemp = 20
+    return temp > coldTemp ? 'warm' : ''
   }
 
   return (
-    <div className="app">
+    <div className={
+      `app ${shouldNotBeEmpty(cityInfo) && shouldWeatherBeWarm(cityInfo.main.temp)}`
+    }>
       <main>
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
-            placeholder="Type in the city name to search..."
+            placeholder="Type and press Enter to search..."
+            onKeyPress={(e) => e.key === 'Enter' ? setCity(e.target.value) : ''}
           />
         </div>
-        <div className="location-box">
-          <div className="location">Taipei, Taiwan</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            25°c
-          </div>
-          <div className="weather">Sunny</div>
-        </div>
+        {shouldNotBeEmpty(cityInfo) && (
+          <>
+            <div className="location-box">
+              <div className="location">
+                {`${cityInfo.name}, ${cityInfo.sys.country}`}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {`${Math.round(cityInfo.main.temp)}°c`}
+              </div>
+              <div className="weather">
+                {cityInfo.weather[0].main}
+              </div>
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
