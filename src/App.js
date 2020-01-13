8bb20cb0d43156e3d4af02ad884f341059400a0f
danny-ch han <chenghsuan@5xruby.tw>
2020-01-13 21:27:48 +0800

    Add .location-box & .weather-box

--- src/App.js
+++ src/App.js
@@ -6,14 +6,59 @@ const api = {
 }
 
 function App() {
+  function dateBuilder(d) {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    const weekdays = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ]
+
+    const day = weekdays[d.getDay()]
+    const date = d.getDate()
+    const month = months[d.getMonth()]
+    const year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
-      <main className="search-box">
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Type in the city name to search..."
-        />
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Type in the city name to search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Taipei, Taiwan</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            25°c
+          </div>
+          <div className="weather">Sunny</div>
+        </div>
       </main>
     </div>
   );
