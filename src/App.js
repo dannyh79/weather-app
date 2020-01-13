37ce7337d6bae1f6852f402e8c1f2310509d6fc2
danny-ch han <chenghsuan@5xruby.tw>
2020-01-13 08:57:13 +0800

    Add search box for Weather App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const api = {
+  key: '664eac603809f1426027912a55140749',
+  url: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Type in the city name to search..."
+        />
+      </main>
     </div>
   );
 }
